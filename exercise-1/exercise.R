4e1a008944945002b108f632feee82e1cacbdf05
applemac <applemac@D-108-179-153-202.dhcp4.washington.edu>
2018-04-03 13:58:02 -0700

    ch8 e1 done

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -1,40 +1,45 @@
 # Exercise 1: creating and accessing lists
 
 # Create a vector `my_breakfast` of everything you ate for breakfast
-
+my_breakfast <- c("egg", "bacon", "rice")
 
 # Create a vector `my_lunch` of everything you ate (or will eat) for lunch
-
+my_lunch <- c("pho", "noodles", "puff")
 
 # Create a list `meals` that has contains your breakfast and lunch
 
-
+meals <- list(my_breakfast, my_lunch)
 # Add a "dinner" element to your `meals` list that has what you plan to eat 
 # for dinner
-
+meals[[3]] <- c("steak", "fries", "bread")
 
 # Use dollar notation to extract your `dinner` element from your list
 # and save it in a vector called 'dinner'
-
+dinner <- meals$dinner
 
 # Use double-bracket notation to extract your `lunch` element from your list
 # and save it in your list as the element at index 5 (no reason beyond practice)
-
+lunch <- meals[[2]]
+meals[[5]] <- lunch
+meals[[2]] <- NULL
 
 # Use single-bracket notation to extract your breakfast and lunch from your list
 # and save them to a list called `early_meals`
 
-
+early_meals <- meals[1:2]
 
 ### Challenge ###
 
 # Create a list that has the number of items you ate for each meal
 # Hint: use the `lappy()` function to apply the `length()` function to each item
 
-
+early_meals_length_list <- lapply(early_meals, length)
 # Write a function `add_pizza` that adds pizza to a given meal vector, and
 # returns the pizza-fied vector
-
+add_pizza <- function(a_vector){
+  a_vector <- c(a_vector, "pizza")
+}
 
 # Create a vector `better_meals` that is all your meals, but with pizza!
 
+better_meals <- lapply(early_meals, add_pizza)
